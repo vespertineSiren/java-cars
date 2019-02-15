2063ed51caba5268c31689e478140c81cc4c3364
Patrick Martin <patrickmartin88@gmail.com>
2019-02-15 10:03:14 -0800

    Added bindings and message converter for Application.

--- src/main/java/com/lambdaschool/javacars/JavacarsApplication.java
+++ src/main/java/com/lambdaschool/javacars/JavacarsApplication.java
@@ -1,7 +1,16 @@
 package com.lambdaschool.javacars;
 
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavacarsApplication {
@@ -12,5 +21,36 @@ public class JavacarsApplication {
         SpringApplication.run(JavacarsApplication.class, args);
     }
 
+    @Bean
+    public TopicExchange appExchange()
+    {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue appQueue()
+    {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding declareBinding()
+    {
+        return BindingBuilder.bind(appQueue()).to(appExchange()).with(QUEUE_NAME);
+    }
+
+    @Bean
+    public RabbitTemplate rt(final ConnectionFactory cf)
+    {
+        final RabbitTemplate rt = new RabbitTemplate(cf);
+        rt.setMessageConverter(producerJackson2MessageConverter());
+        return rt;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter()
+    {
+        return new Jackson2JsonMessageConverter();
+    }
 }
 
