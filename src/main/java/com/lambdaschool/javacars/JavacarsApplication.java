491fcd256535d650ab6e609a0c5b14e2360b111f
Patrick Martin <patrickmartin88@gmail.com>
2019-02-15 09:02:02 -0800

    Initial Commit

---
+++ src/main/java/com/lambdaschool/javacars/JavacarsApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.javacars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacarsApplication.class, args);
+    }
+
+}
+
