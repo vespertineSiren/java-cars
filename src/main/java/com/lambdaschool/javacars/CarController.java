b2860e381744ab47fe2f4b7c3acf13b6ffdc5ccb
Patrick Martin <patrickmartin88@gmail.com>
2019-02-15 10:15:55 -0800

    Added car controller constructor and LogConsumer.

--- src/main/java/com/lambdaschool/javacars/CarController.java
+++ src/main/java/com/lambdaschool/javacars/CarController.java
@@ -1,4 +1,21 @@
 package com.lambdaschool.javacars;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
 public class CarController {
+
+    private final CarRepository carrepo;
+    private final RabbitTemplate rt;
+
+    public CarController(CarRepository carrepo, RabbitTemplate rt) {
+        this.carrepo = carrepo;
+        this.rt = rt;
+    }
+
+
+
 }
--- src/main/java/com/lambdaschool/javacars/LogConsumer.java
+++ src/main/java/com/lambdaschool/javacars/LogConsumer.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.javacars;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class LogConsumer {
 
-
+    @RabbitListener(queues = JavacarsApplication.QUEUE_NAME)
+    public void consumeMessage(final Message cm){
+        log.info("Received message: {}", cm.toString());
+    }
 }
